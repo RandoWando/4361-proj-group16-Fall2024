91463bb51ee736daf744522463b8a2f706ea60fb
Demarcus Braclet <demarcusbraclet@outlook.com>
2024-12-03 22:13:33 -0600

    Fixed button press

--- Assets/lastTryPt2.cs
+++ Assets/lastTryPt2.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.InputSystem;
 namespace Shmoove
 {
     public class lastTryPt2 : MonoBehaviour
     {
+        public void Update()
+        {
+            // locking cursor
+            Cursor.lockState = CursorLockMode.None;
+        }
         public void ToMainMenu()
         {
             SceneManager.LoadScene("Level 1- ashton");
